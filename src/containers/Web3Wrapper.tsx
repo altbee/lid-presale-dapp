b5a2663016289d4078c8ce63169eab3ba437e565
Kyle Longrich <longrichk@gmail.com>
2020-09-19 13:26:58 -0400

    fixed address updating when selected in meta mask

--- src/containers/Web3Wrapper.tsx
+++ src/containers/Web3Wrapper.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
 import { HttpProvider, IpcProvider, WebsocketProvider } from 'web3-core';
 import { getRandomInfuraId, web3Modal } from 'utils';
+import { callbackify } from 'util';
 
 interface IWeb3Wrapper {
   children: (
@@ -16,13 +17,14 @@ type Web3Provider = HttpProvider | IpcProvider | WebsocketProvider;
 const Web3Wrapper: React.FC<IWeb3Wrapper> = ({ children }) => {
 
   const [address, setAddress] = useState('');
+
   const [provider, setProvider] = useState<Web3Provider | null>(
     new Web3.providers.HttpProvider(
       `https://mainnet.infura.io/v3/${getRandomInfuraId()}`
     )
 
   );
-  
+
   const [web3, setWeb3] = useState<Web3 | null>(new Web3(provider));
 
   // const resetApp = async () => {
@@ -56,11 +58,19 @@ const Web3Wrapper: React.FC<IWeb3Wrapper> = ({ children }) => {
   // };
 
   const onConnect = async () => {
+
     const provider = await web3Modal.connect();
     const web3 = await new Web3(provider);
-    // await subscribeProvider(provider, web3);
+    // await subscribeProvider(provider, web3); 
     const accounts = await web3.eth.getAccounts();
     const address = accounts[0];
+
+    const web2 = await window.web3;    
+    web2.currentProvider.publicConfigStore.on('update',  function(callback: any) {
+      const address = callback.selectedAddress;
+
+      setAddress(address);
+    });
 
     setAddress(address);
     setProvider(provider);
@@ -69,6 +79,7 @@ const Web3Wrapper: React.FC<IWeb3Wrapper> = ({ children }) => {
 
   useEffect(() => {
     if (window.web3) onConnect();
+
   }, []);
 
   return <>{children(address, web3, onConnect)}</>;
