83665179c94424cdd426bfa6cb2c7e545add044d
Kyle Longrich <longrichk@gmail.com>
2020-09-22 11:12:18 -0400

    fixes to display different text when timer ends

--- src/components/EndTimer.tsx
+++ src/components/EndTimer.tsx
@@ -3,13 +3,30 @@ import { Text, Box } from '@chakra-ui/core';
 import { fromWei } from 'utils';
 import CountDown from './CountDown';
 import { tokenName } from '../config';
+import { useState, useEffect } from 'react';
+import { useTimer } from 'react-timer-hook';
+
 
 interface IEndTimer {
-  expiryTimestamp: number | null;
-  hardcap: string;
+  expiryTimestamp: any;
+  hardcap: any;
 }
 
 const EndTimer: React.FC<IEndTimer> = ({ expiryTimestamp, hardcap }) => {
+  const [timerLive, setTimelive] = useState(true);
+
+  const { seconds, minutes, hours, days, start, restart } = useTimer({
+    expiryTimestamp,
+    onExpire: () => setTimelive(false),
+  });
+
+  useEffect(() => {
+    restart(expiryTimestamp);
+    start();
+  }, [expiryTimestamp]);
+
+
+  if (timerLive) {
   return (
     <Box
       display="block"
@@ -29,7 +46,7 @@ const EndTimer: React.FC<IEndTimer> = ({ expiryTimestamp, hardcap }) => {
         expiryTimestamp={
           expiryTimestamp === null ? Date.now() : expiryTimestamp
         }
-      />
+      /> 
       <Text fontSize={['12px', '14px']} fontWeight="light" mt="-20px">
         48 hour timer.
       </Text>
@@ -38,6 +55,30 @@ const EndTimer: React.FC<IEndTimer> = ({ expiryTimestamp, hardcap }) => {
       </Text>
     </Box>
   );
+  } else {
+    return (
+      <Box
+      display="block"
+      w="100%"
+      mt="40px"
+      mb="40px"
+      px={['20px', '20px', 0]}
+      maxW="1200px"
+      ml="auto"
+      mr="auto"
+      textAlign="center"
+    >
+      <Text fontSize={['28px', '36px']} fontWeight="bold">
+        {/* Put Logic or what to show here */}
+
+        All CXN Aviable To Claim
+      
+      </Text>
+      
+    </Box>
+
+    )
+  }
 };
 
 export default EndTimer;
